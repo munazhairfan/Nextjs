f0a35975a9bae100cc0f93e66ad1d49bb7c7f706
munazhairfan <munazhairfan@gmail.com>
2024-10-11 01:12:17 +0500

    Add files via upload

---
+++ step-01/app/service/page.tsx
@@ -0,0 +1,10 @@
+export default function Service(){
+    return(
+        <>
+        <div style={{
+            backgroundImage:"url(./background.jpg)",
+            fontSize:"5rem",
+            height:"200px"}}>HI! I AM A SERVICE PAGE!</div>
+        </>
+    )
+}
