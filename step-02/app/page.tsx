1ab5e3b03dda4c7ca9d962900c4e2a0100e2c39b
munazhairfan <munazhairfan@gmail.com>
2024-10-19 18:49:56 +0500

    page.tsx

--- step-02/app/page.tsx
+++ step-02/app/page.tsx
@@ -5,11 +5,6 @@ import Footer from "./components/Footer/Footer";
 export default function HomePage(){
   return(
     <>
-    <head>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-    />
     </head>
     <Header/>
     <HeroSection/>
