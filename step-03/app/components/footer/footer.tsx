a078c9b34f27dca60acbfa812b9edc162d169c49
munazhairfan <munazhairfan@gmail.com>
2024-10-21 00:06:26 +0500

    Delete step-03 directory

--- step-03/app/components/footer/footer.tsx
+++
@@ -1,6 +0,0 @@
-export default function Footer(){
-    return(
-        <p className="bg-zinc-400 text-white h-12 content-center text-center">
-            &copy; 2024 Munazha Irfan Ahmed. All rights reserved.</p>
-    )
-}
